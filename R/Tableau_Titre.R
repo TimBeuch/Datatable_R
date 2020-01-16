881e9925841b84f937c8721800187603cb10222c
TimBeuch <57632842+TimBeuch@users.noreply.github.com>
2020-01-16 15:58:43 +0100

    Add files via upload

--- R/Tableau_Titre.R
+++ R/Tableau_Titre.R
@@ -13,25 +13,26 @@ TABLEAU_TITRE <- function(data = data,
                           color = color,
                           ...){
 
-  toto = paste('caption-side: top;
-               text-align: center;
-               color:',
-               color,
-               ';font-size: 2em;
-               text-decoration: underline;
-               margin-top: 15px;
-               margin-bottom: 15px;',
-               sep = "")
+  # toto = paste('caption-side: top;
+  #              text-align: center;
+  #              color:',
+  #              color,
+  #              ';font-size: 2em;
+  #              text-decoration: underline;
+  #              margin-top: 15px;
+  #              margin-bottom: 15px;',
+  #              sep = "")
 
-  tata = paste('font-size: 1em;',
-               sep = "")
+  toto = glue::glue("caption-side: top;text-align: center;color:{color};font-size: 2em; text-decoration: underline;margin-top: 15px; margin-bottom: 15px;")
+
+  tata = paste0('font-size: 1em;')
 
   if (is.null(number) == TRUE &
       !is.null(title) == TRUE){
 
     title = htmltools::tags$caption(style = toto,
                                     htmltools::em(style = tata,
-                                                  title))
+                                                  glue::glue(title)))
   } else if (!is.null(number) == TRUE &
              is.null(title) == TRUE){
 
@@ -48,7 +49,7 @@ TABLEAU_TITRE <- function(data = data,
                                           number,
                                           ":",
                                           sep = " "),
-                                    htmltools::em(title))
+                                    htmltools::em(glue::glue(title)))
   } else {title = NULL}
 
   return(title)
